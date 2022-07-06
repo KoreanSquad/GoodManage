9d393b7f2056dd53674180b62b80f99611174d91
David <scriptura7773@gmail.com>
2022-07-06 16:03:38 -0700

    added error handler

--- server/server.js
+++ server/server.js
@@ -1,6 +1,31 @@
 const express = require('express');
-
+const mainRouter = ('')
 const PORT = 3000;
 
+
 const app = express();
+
 app.use(express.json());
+
+app.use('/api', mainRouter)
+
+// Global Error Catcher
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+  
+  // Start Server
+  app.listen(PORT, () => {
+      console.log(`Server listening on port: ${PORT}...`);
+    });
+  
+  
+    
+  module.exports = app;
